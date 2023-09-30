34654545c5ba681a420a4c33bf4b4169c3402954
Nikolay Shepelev <shepelevnikolay97@gmail.com>
2023-09-30 15:14:39 +0300

    Add basename to CreateBrowserRouter

--- Project/src/components/Routes/Routes.tsx
+++ Project/src/components/Routes/Routes.tsx
@@ -6,32 +6,35 @@ import { NotFoundPage } from './NotFoundPage';
 import { BaseLayout } from 'components/BaseLayout';
 
 export function Routes() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: (
-        <BaseLayout>
-          <main aria-live="assertive">
-            <Outlet />
-          </main>
-        </BaseLayout>
-      ),
-      children: [
-        {
-          path: '/',
-          element: <TimerPage />,
-        },
-        {
-          path: '/statistics',
-          element: <StatisticsPage />,
-        },
-        {
-          path: '/*',
-          element: <NotFoundPage />,
-        },
-      ],
-    },
-  ]);
+  const router = createBrowserRouter(
+    [
+      {
+        path: '/',
+        element: (
+          <BaseLayout>
+            <main aria-live="assertive">
+              <Outlet />
+            </main>
+          </BaseLayout>
+        ),
+        children: [
+          {
+            path: '/',
+            element: <TimerPage />,
+          },
+          {
+            path: '/statistics',
+            element: <StatisticsPage />,
+          },
+          {
+            path: '/*',
+            element: <NotFoundPage />,
+          },
+        ],
+      },
+    ],
+    { basename: '/pomodoro-box-testing' }
+  );
 
   return <RouterProvider router={router} />;
 }
